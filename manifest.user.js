622bb72b8d48b7ebc6635060915f463e7af4ad07
JorikDura <vandarkslave@outlook.com>
2025-08-14 00:26:20 +0300

    in work

--- manifest.user.js
+++ manifest.user.js
@@ -4,7 +4,7 @@
 // @description Возвращает удаленные жанры с сайта shikimori
 // @author chumyshhh
 // @homepageURL https://github.com/JorikDura/Shikimori-Genres
-// @version 3.0.5
+// @version 1
 // @copyright 2025, chumyshhh
 // @license GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
 
